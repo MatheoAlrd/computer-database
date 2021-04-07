ebe9e1d7792e146d780d8e42ec378007b4c43a3b
Matheo ALLARD <matheo.allard1@hotmail.fr>
2021-04-07 16:01:06 +0200

    Deletion Company is done Nothing from jdbcTemplate is working, but at least it compiles :)

--- src/main/java/com/excilys/cdb/model/dao/CompanyDAO.java
+++ src/main/java/com/excilys/cdb/model/dao/CompanyDAO.java
@@ -1,27 +1,21 @@
 
 package com.excilys.cdb.model.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Types;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.excilys.cdb.model.dto.CompanyDTO;
-import com.excilys.cdb.model.mapper.CompanyMapper;
 
 @Component
 public class CompanyDAO {
@@ -31,12 +25,9 @@ public class CompanyDAO {
 	
 	private static final String CREATE_QUERY = "INSERT INTO company (name) VALUES (:name)";
 	
-	private static final String DELETE_COMPUTERS_BY_COMPANY_QUERY = "DELETE FROM computer WHERE company_id = ?";
-	private final static String DELETE_COMPANY_QUERY = "DELETE FROM company WHERE id = ?";
+	private static final String DELETE_COMPUTERS_BY_COMPANY_QUERY = "DELETE FROM computer WHERE company_id = :id";
+	private final static String DELETE_COMPANY_QUERY = "DELETE FROM company WHERE id = :id";
 	
-	private static final String SET_AUTOCOMMIT = "SET autocommit = 0";
-	private static final String ROLLBACK = "ROLLBACK";
-	private static final String COMMIT = "COMMIT";
 
 	protected static Logger logger = LoggerFactory.getLogger(CompanyDAO.class);
 
@@ -69,28 +60,14 @@ public class CompanyDAO {
 		new NamedParameterJdbcTemplate(datasource).update(CREATE_QUERY, params);
 	}
 	
+	@Transactional
 	public void delete(int id) {
-		try (Connection con = DataSource2.getConnection()){
-			
-			con.createStatement().executeQuery(SET_AUTOCOMMIT);
-
-			PreparedStatement ps = con.prepareStatement(DELETE_COMPUTERS_BY_COMPANY_QUERY, Statement.RETURN_GENERATED_KEYS);
-			ps.setInt(1, id);
-			ps.executeUpdate();
-			
-			ps = con.prepareStatement(DELETE_COMPANY_QUERY, Statement.RETURN_GENERATED_KEYS);
-			ps.setInt(1, id);
-			ps.executeUpdate();
-			
-			con.createStatement().executeQuery(COMMIT);			
-		} catch(Exception e1) {
-			try {
-				logger.warn("Something didn't work in the transaction, start of the rollback" + e1.getMessage());
-				DataSource2.getConnection().createStatement().executeQuery(ROLLBACK);
-
-			} catch (SQLException e2) {
-				logger.error("Rollback didn't work " + e2.getMessage());
-			}
-		}
+		
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("id", id, Types.VARCHAR);
+	
+		NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(datasource);
+		jdbcTemplate.update(DELETE_COMPUTERS_BY_COMPANY_QUERY, params);
+		jdbcTemplate.update(DELETE_COMPANY_QUERY, params);
 	}
 }
